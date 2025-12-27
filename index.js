f08d98358f1ccf2dcfecd95ec2a3cef0a4172709
ahmedsami019 <easami019192@gmail.com>
2025-12-27 19:32:05 +0600

    get coffee data succesfully

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const app = express()
-const port = process.env.PORT || 3003
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 3003;
 
-app.use(cors())
-app.use(express.json())
-
-console.log(process.env.DB_PASS);
-console.log(process.env.DB_USER);
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@maincluster0.m4dyknx.mongodb.net/?appName=MainCluster0`;
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -20,30 +16,46 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // coffees collection
+    const coffeeCollection = client.db("espresso-coffee-store").collection("coffees");
+
+    // all get method
+    app.get('/coffees', async(req, res)=>{
+        const result = await coffeeCollection.find().toArray()
+        res.send(result);
+    })
+
+    // all post method
+    app.post("/coffees", async (req, res) => {
+      const newCoffee = req.body;
+      console.log(newCoffee);
+      const result = await coffeeCollection.insertOne(newCoffee);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+
   }
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("coffee server is becoming warmer");
+});
 
-
-app.get('/', (req, res) => {
-    res.send("coffee server is becoming warmer");
-})
-
-app.listen(port, ()=>{
-    console.log(`the server is running on port : ${port}`);
-})
-
+app.listen(port, () => {
+  console.log(`the server is running on port : ${port}`);
+});
