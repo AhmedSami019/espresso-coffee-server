aef35f1fe4a78a884528ac3111b48be0853c0c25
ahmedsami019 <easami019192@gmail.com>
2025-12-27 23:42:20 +0600

    get single data api added

--- index.js
+++ index.js
@@ -33,10 +33,17 @@ async function run() {
         res.send(result);
     })
 
+    app.get('/coffees/:id', async(req, res)=>{
+        const id = req.params.id
+        const query = {_id : new ObjectId(id)}
+        const result = await coffeeCollection.findOne(query)
+        res.send(result)
+    })
+
     // all post method
     app.post("/coffees", async (req, res) => {
       const newCoffee = req.body;
-      console.log(newCoffee);
+    //   console.log(newCoffee);
       const result = await coffeeCollection.insertOne(newCoffee);
       res.send(result);
     });
@@ -44,6 +51,7 @@ async function run() {
     // all delete function
     app.delete("/coffees/:id", async(req, res) =>{
         const id = req.params.id
+        // console.log(id);
         const query = {_id : new ObjectId(id)}
         const result = await coffeeCollection.deleteOne(query)
         res.send(result)
